a7070edb9073e969545af36f85a20678c39e4e77
Suraj Giri <155833064+surajixcp@users.noreply.github.com>
2025-02-06 12:16:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+document.querySelectorAll('.photo-album img').forEach(img => {
+    img.addEventListener('click', function() {
+        const mainImage = document.querySelector('.photo-main img');
+        mainImage.src = this.src;
+    });
+});
+
